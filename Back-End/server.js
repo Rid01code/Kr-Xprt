2110115aa1461c2b882f03d9d789e288ff02f401
Ridwan Rahaman <itsridofficcial@gmail.com>
2024-10-23 00:05:15 +0530

    fixed vercel.json 5

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path")
-require("./Connection/Connection")
-const patientApi = require("./Routes/Patients")
-const doctorApi = require("./Routes/Doctors")
+const path = require("path");
+require("./Connection/Connection");
+const patientApi = require("./Routes/Patients");
+const doctorApi = require("./Routes/Doctors");
 
-const port = 8080
-
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../Front-End/dist')));
 
-app.use("/patient" , patientApi)
-app.use("/doctor" ,  doctorApi)
+app.use("/patient", patientApi);
+app.use("/doctor", doctorApi);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, '../Front-End/dist/index.html'));
+});
 
-app.listen(port ,()=>{
-    console.log(`listening to the port ${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+    const port = 8080;
+    app.listen(port, () => {
+        console.log(`listening to the port ${port}`);
+    });
+}
+
+module.exports = app;
